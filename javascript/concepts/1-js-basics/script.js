4657f9541482df25668ca6f422a3f43e5ea7d3c2
Adi Reddy <syagamreddyadireddy423@gmail.com>
2025-08-28 11:00:01 +0530

    Added Practiced files

---
+++ javascript/concepts/1-js-basics/script.js
@@ -0,0 +1,19 @@
+var name = "adi";
+let age = 21;
+const loc = "hyd";
+
+console.log("Name: ", name);
+console.log("Age: ", age);
+console.log("Location: ", loc);
+
+let a = "123";
+let b = Number(a);
+let c = b + 10;
+
+console.log("Result :",c);
+console.log(Boolean(c));
+
+let d = "Adi";
+let e = 3;
+
+console.log( typeof (d + e));
