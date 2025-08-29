b14afe3db165f391835ef910c11a99a44ddb5209
Adi Reddy <syagamreddyadireddy423@gmail.com>
2025-08-29 19:03:38 +0530

    Practiced Operators and Conditions

---
+++ javascript/concepts/2-js-basics/script.js
@@ -0,0 +1,28 @@
+let marks = 67;
+
+if(marks >= 90) {
+console.log("Grade: A");
+}
+else if(marks >=70) {
+console.log("Grade: B");
+}
+else {
+console.log("Geade: C");
+}
+
+
+let grade = "B";
+
+switch(grade) {
+case "A":
+console.log("Good");
+break;
+case "B":
+console.log("Average");
+break;
+case "C":
+console.log("Below Avg");
+break;
+default:
+console.log("Fail");
+}
