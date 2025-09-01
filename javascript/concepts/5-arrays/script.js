a03f0f839b2db38ed41ffb9a23b5bacd38d87338
Adi Reddy <syagamreddyadireddy423@gmail.com>
2025-09-01 08:12:14 +0530

    Practiced : Arrays

---
+++ javascript/concepts/5-arrays/script.js
@@ -0,0 +1,26 @@
+let arr = [10,20,30];
+console.log(arr);
+arr.push(40);
+arr.pop();
+
+
+let arr1 = [1,2,3,4];
+console.log(arr1);
+arr1.shift();
+arr1.unshift(0);
+
+
+let arr2 = ["adi","asmi","riyansh"];
+console.log(arr2.indexOf("adi"));
+console.log(arr2.indexOf("anitha"));
+
+
+let arr3 = [1,2,3,4,5];
+console.log(arr3.slice(2));
+
+
+let arr4 = [1,2,3,4,5,6]
+arr4.splice(2,2);
+console.log(arr4);
+arr4.splice(1,0,9);
+console.log(arr4);
